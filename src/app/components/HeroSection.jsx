434c8d0da573e7a28979ba4de2c2ace4699ccd0d
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-19 22:29:41 -0600

    added type animation and styles to font

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,10 +1,42 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client";
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
-      <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-        Hello, I'm Tiesha
+      <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-pink-600">
+          Hello, I'm{" "}
+        </span>
+        <br></br>
+        <TypeAnimation
+          sequence={[
+            "Tiesha",
+            1000,
+            "Front End Web Developer",
+            1000,
+            "HTML",
+            1000,
+            "CSS",
+            1000,
+            "Javascript",
+            1000,
+            "React",
+            1000,
+            "Node.JS",
+            1000,
+            "Next.JS",
+            1000,
+            "Bootstrap",
+            1000,
+            "Tailwind",
+          ]}
+          wrapper="span"
+          speed={50}
+          repeat={Infinity}
+        />
       </h1>
       <p className="text-[#ADB7BE] text-lg lg:text-xl">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, numquam.
