45229fcfb1ef285e163189e7bd8f955aa9c306a6
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-20 00:26:28 -0600

    Added link

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+"use client";
 import Link from "next/link";
+import NavLink from "./NavLink";
+import React, { useState } from "react";
+
+const navLinks = [
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
+];
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
@@ -11,7 +29,9 @@ const Navbar = () => {
         >
           LOGO
         </Link>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
