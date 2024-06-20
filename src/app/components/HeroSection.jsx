f2d7521a30668c3e75431680e7094e96aaf50855
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-19 20:38:38 -0600

    Added hero section

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/no-unescaped-entities */
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+        Hello, I'm Tiesha
+      </h1>
+      <p className="text-[#ADB7BE] text-lg lg:text-xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, numquam.
+        Aspernatur, amet nisi eum qui quis placeat eveniet. Recusandae officia
+        doloribus nobis. Voluptas nisi qui incidunt consectetur neque at
+        accusamus?
+      </p>
+    </section>
+  );
+};
+
+export default HeroSection;
