c81aae6ba81cac1bb05c4bcf14222f32f2bff792
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-21 02:45:47 -0600

    Modified styles

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-4xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-pink-600">
               Hello, I'm{" "}
             </span>
@@ -18,7 +18,6 @@ const HeroSection = () => {
               sequence={["Tiesha", 1000, "Web Developer", 1000]}
               wrapper="span"
               speed={50}
-              style={{ fontSize: "1em", display: "inline-block" }}
               repeat={Infinity}
             />
           </h1>
@@ -35,16 +34,16 @@ const HeroSection = () => {
               Download CV
             </span>
           </button>
-          <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-            <div className=" rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-              <Image
-                src="/images/memoji-laptop.png"
-                alt="memoji-with-laptop"
-                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                width={300}
-                height={300}
-              />
-            </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className=" rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/memoji-laptop.png"
+              alt="memoji-with-laptop"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={500}
+              height={500}
+            />
           </div>
         </div>
       </div>
