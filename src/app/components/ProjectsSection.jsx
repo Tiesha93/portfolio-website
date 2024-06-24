62f6110abefde1bc86dd02d06b7ad2a473f5175e
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-23 21:17:52 -0600

    Modified styles

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
   {
@@ -76,11 +77,18 @@ const ProjectsSection = () => {
       const filteredProjects = projectsData.filter((project) =>
         project.tag.includes(tag)
       );
+        const cardVariants = {
+          initial: { y: 50, opacity: 0 },
+          animate: { y: 0, opacity: 1 },
+        };
+
 
 
   return (
     <section id="projects">
-      <h2>My Projects</h2>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
           onClick={handleTagChange}
@@ -100,6 +108,13 @@ const ProjectsSection = () => {
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={useInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
             <ProjectCard
               key={project.id}
               title={project.title}
@@ -108,8 +123,8 @@ const ProjectsSection = () => {
               gitUrl={project.gitUrl}
               previewUrl={project.previewUrl}
             />
-          )
-        )}
+          </motion.li>
+        ))}
       </ul>
     </section>
   );
