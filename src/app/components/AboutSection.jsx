c62d72487fe96053b6e5f973cd9610df2bcbf13f
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-23 18:19:00 -0600

    Added skills to about me section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,10 +1,33 @@
 "use client";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Typescript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Next.js</li>
+        <li>Git</li>
+        <li>Bootstrap</li>
+        <li>Tailwind</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
+
   return (
     <section className="text-white" id="About">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
@@ -20,8 +43,8 @@ const AboutSection = () => {
             I am a front end web developer with a passion for creating
             interactive and responsive web applications. I have experience
             working with JavaScript, Git, React, Node.js, Next.js, Typescript,
-            JSON, HTML, CSS, Bootstrap, and Tailwind. I am a quick learner and I
-            am always looking to expand my knowledge and skill set. I am a team
+            HTML, CSS, Bootstrap, and Tailwind. I am a quick learner and I am
+            always looking to expand my knowledge and skill set. I am a team
             player and I am excited to work with others to create amazing
             applications.
           </p>
