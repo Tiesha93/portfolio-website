8c06fbe362c2d71e8141e567213b0b5abaca363a
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-23 19:57:28 -0600

    Added descriptionand images to project data array

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -4,57 +4,57 @@ import ProjectCard from "./ProjectCard";
 const projectsData = [
   {
     id: 1,
-    title: "",
-    description: "",
-    image: "",
-    tag: [""],
+    title: "Landing Page",
+    description: "This is my first project, a simple langing page with HTML and CSS flexbox",
+    image: "/images/projects/landing-page.png",
+    tag: ["All", "Web"],
     gitUrl: "/",
-    previewUrl: "",
+    previewUrl: "/",
   },
   {
     id: 2,
-    title: "",
-    description: "",
-    image: "",
-    tag: [""],
+    title: "Tic-Tac-Toe",
+    description: "This is the game of tic-tac-toe. I builded this project using HTML, CSS, and JavaScript",
+    image: "/images/projects/tic-tac-toe.png",
+    tag: ["All", "Web"],
     gitUrl: "/",
-    previewUrl: "",
+    previewUrl: "/",
   },
   {
     id: 3,
-    title: "",
-    description: "",
-    image: "",
-    tag: [""],
+    title: "Calculator",
+    description: "This is a simple calculator. Build with HTML, CSS, and JavaScript.",
+    image: "/images/projects/calculator.png",
+    tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "",
   },
   {
     id: 4,
-    title: "",
-    description: "",
-    image: "",
-    tag: [""],
-    gitUrl: "",
-    previewUrl: "",
+    title: "Astrology API",
+    description: "This app shows the current astronomy of any location. It uses OpenWeatherMap API to fetch the data.",
+    image: "/images/projects/api.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 5,
-    title: "",
-    description: "",
-    image: "",
-    tag: [""],
-    gitUrl: "",
-    previewUrl: "",
+    title: "Shopping Cart",
+    description: "This is an eCommerce website built with React.js. Users can browse the available products and give orders with a fully functional cart.",
+    image: "/images/projects/shopping-cart.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 6,
-    title: "",
-    description: "",
-    image: "",
-    tag: [""],
-    gitUrl: "",
-    previewUrl: "",
+    title: "Hang Man",
+    description: "This is a classic word guessing game where players try to guess a hidden word by suggesting letters within a certain number of attempts.",
+    image: "/images/projects/hangman.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
 ];
 
@@ -63,7 +63,7 @@ const ProjectsSection = () => {
     <div>
       <h2>My Projects</h2>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectsSection
+export default ProjectsSection;
