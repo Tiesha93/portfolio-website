8778b7f14892a06fb95734bc58a1a2a160965528
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2024-06-23 21:11:05 -0600

    Modified styles

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
     id: 1,
     title: "Landing Page",
-    description: "This is my first project, a simple langing page with HTML and CSS flexbox",
+    description:
+      "This is my first project, a simple langing page with HTML and CSS flexbox",
     image: "/images/projects/landing-page.png",
     tag: ["All", "Web"],
     gitUrl: "/",
@@ -14,7 +17,8 @@ const projectsData = [
   {
     id: 2,
     title: "Tic-Tac-Toe",
-    description: "This is the game of tic-tac-toe. I builded this project using HTML, CSS, and JavaScript",
+    description:
+      "This is the game of tic-tac-toe. I builded this project using HTML, CSS, and JavaScript",
     image: "/images/projects/tic-tac-toe.png",
     tag: ["All", "Web"],
     gitUrl: "/",
@@ -23,7 +27,8 @@ const projectsData = [
   {
     id: 3,
     title: "Calculator",
-    description: "This is a simple calculator. Build with HTML, CSS, and JavaScript.",
+    description:
+      "This is a simple calculator. Build with HTML, CSS, and JavaScript.",
     image: "/images/projects/calculator.png",
     tag: ["All", "Web"],
     gitUrl: "/",
@@ -32,7 +37,8 @@ const projectsData = [
   {
     id: 4,
     title: "Astrology API",
-    description: "This app shows the current astronomy of any location. It uses OpenWeatherMap API to fetch the data.",
+    description:
+      "This app shows the current astronomy of any location. It uses OpenWeatherMap API to fetch the data.",
     image: "/images/projects/api.png",
     tag: ["All", "Web"],
     gitUrl: "/",
@@ -41,7 +47,8 @@ const projectsData = [
   {
     id: 5,
     title: "Shopping Cart",
-    description: "This is an eCommerce website built with React.js. Users can browse the available products and give orders with a fully functional cart.",
+    description:
+      "This is an eCommerce website built with React.js. Users can browse the available products and give orders with a fully functional cart.",
     image: "/images/projects/shopping-cart.png",
     tag: ["All", "Web"],
     gitUrl: "/",
@@ -50,7 +57,8 @@ const projectsData = [
   {
     id: 6,
     title: "Hang Man",
-    description: "This is a classic word guessing game where players try to guess a hidden word by suggesting letters within a certain number of attempts.",
+    description:
+      "This is a classic word guessing game where players try to guess a hidden word by suggesting letters within a certain number of attempts.",
     image: "/images/projects/hangman.png",
     tag: ["All", "Web"],
     gitUrl: "/",
@@ -59,20 +67,51 @@ const projectsData = [
 ];
 
 const ProjectsSection = () => {
+      const [tag, setTag] = useState("All");
+      const ref = useRef(null);
+      const handleTagChange = (newTag) => {
+        setTag(newTag);
+      };
+
+      const filteredProjects = projectsData.filter((project) =>
+        project.tag.includes(tag)
+      );
+
+
   return (
-    <div>
+    <section id="projects">
       <h2>My Projects</h2>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        <ProjectCard
-          key={project.id}
-          title={project.title}
-          description={project.description}
-          imgUrl={project.image}
-          gitUrl={project.gitUrl}
-          previewUrl={project.previewUrl}
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
         />
       </div>
-    </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          )
+        )}
+      </ul>
+    </section>
   );
 };
 
